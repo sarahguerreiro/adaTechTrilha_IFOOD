055f559ae5ee241f53304df1bb3480602be79b84
Sarah <sdmguerre@gmail.com>
2023-11-13 17:28:37 -0300

    Finalizando os ultimos 2 exercicios passados na aula anterior

--- logica_de_prog/aula6/numerosPares.js
+++ logica_de_prog/aula6/numerosPares.js
@@ -1,18 +1,43 @@
+/*do while*/
+
+let totalPares = 0;
+let totalImpares = 0;
+let stop = 1;
+
+do {
+  const numero = Number(prompt('Digite um numero'))
+  stop = numero
+
+  if(numero % 2 === 0) {
+    totalPares = totalPares + numero;
+  } else {
+    totalImpares = totalImpares + numero;
+  }
+} while (stop !== 0);
+
+console.log('Total das somas dos pares:', totalPares)
+console.log('Total das somas dos Impares:', totalImpares)
+console.log('Total:', totalImpares + totalPares)
+
+
+/* While */
+
 let totalPares = 0;
 let totalImpares = 0;
 
 while(true){
-  const numero = Number(prompt("digite um numero"))
-  if(numero === 0){
+  const numero = Number(prompt('Digite um numero'))
+  if(numero === 0) {
     break;
   }
+
   if(numero % 2 === 0) {
-  total = total + numero
+    totalPares = totalPares + numero;
   } else {
-    
+    totalImpares = totalImpares + numero;
   }
 }
 
-console.log(`total das somas dos pares: `, total)
-console.log(`total das somas dos impares: `, total)
- 
+console.log('Total das somas dos pares:', totalPares)
+console.log('Total das somas dos Impares:', totalImpares)
+console.log('Total:', totalImpares + totalPares) 
