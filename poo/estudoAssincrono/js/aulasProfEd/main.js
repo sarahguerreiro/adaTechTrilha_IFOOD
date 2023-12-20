b117263547069d319b2464af6b71cf3d99815158
Sarah <sdmguerre@gmail.com>
2023-12-20 19:57:11 -0300

    cria a class PessoaController e importa ela para o main

--- poo/estudoAssincrono/js/aulasProfEd/main.js
+++ poo/estudoAssincrono/js/aulasProfEd/main.js
@@ -1,61 +1,20 @@
-// IMPORT e EXPORT são recursos do js para linkar classes a arquivos princiais.
+//import { Pessoa } from "./Pessoa";
+import { PessoaController } from "../js/aulasProfEd/PessoaController.js";
 
-// IMPORT = Depois da palavra reservada import é passado, entre chaves o nome da class, depois a palavra reservada from e, entre aspas, o caminho/edereço do arquivo que carrega esta class:
+let pessoaController = new PessoaController();
 
-import { Pessoa } from "../aulasProfEd/Pessoa.js";
+// CONTROLAR ENVIO DO FORMULARIO E EXIBIÇÃO NA TABELA
 
-//é possível passar vários import, basta compiar a linha e setar os parametros
+let form = document.querySelector('form')
 
-// CRIAR NOVO OBJETO A PARTIR DA CLASS: vc tem uma variavel chamada pessoa1 que será um novo objeto do tipo Pessoa ou uma nova instancia de Pessoa(MANEIRA RECOMENDADA): 
-let pessoa1 = new Pessoa('Herbert Biana', 78, 80.5, 1.65);
-let pessoa2 = new Pessoa('Anderson Silva', 4, 6.5, 30);
-let pessoa3 = new Pessoa('Branca da Silva', 4, 4.6, 20);
-let pessoa4 = new Pessoa('Gilberta da Silva', 4, 4.9, 15);
-let pessoa5 = new Pessoa('Amy Winehouse', 27, 50, 1.59, 15);
+//escutador de evento no formulario
 
-console.log(Pessoa.totalPessoas)
+form.addEventListener('submit', (event) => {
 
-console.log('Nome: ' + pessoa1.nome)
-console.log('Idade: ' + pessoa1.idade)
-console.log('IMC: ' + pessoa1.imc)
+    //adicionar pesssoa
+    pessoaController.adiciona(event)
 
-pessoa1.nome = 'sarah guerreiro'
-pessoa1._idade = 30
-pessoa1.peso = 54
-
-console.log('Nome: ' + pessoa1.nome)
-console.log('Idade: ' + pessoa1.idade)
-console.log('Peso: ' + pessoa1.peso)
-
-console.log(pessoa1.classificaImc())
-console.log(pessoa2.classificaImc())
-console.log(pessoa3.classificaImc())
-console.log(pessoa4.classificaImc())
-console.log(pessoa5.classificaImc())
-/*
-// ATRIBUIR VALORES PARA ATRIBUTOS DE UM OBJETO(MANEIRA NAO RECOMENDADA):
-pessoa1._nome = 'Sarah'
-pessoa1._idade = 27
-pessoa1._peso = 70
-pessoa1._altura = 1.68
-//pessoa1.imc = peso / Math.pow(altura, 2)
-pessoa1._sexo = 'F'
-
-pessoa2._nome = 'Everton'
-pessoa2._idade = 33
-pessoa2._peso = 80
-pessoa2._altura = 1.70
-//pessoa2.imc = pessoa1.peso / pessoa1.altura.Math.pow(altura, 2)
-pessoa2._sexo = 'M'
-
-pessoa3._nome = 'Isabelle'
-pessoa3._idade = 12
-pessoa3._peso = 37
-pessoa3._altura = 1.48
-//pessoa3.imc = peso / Math.pow(altura, 2)
-pessoa3._sexo = 'F'
-*/
-
-console.log(pessoa1)
-console.log(pessoa2)
-console.log(pessoa5)
+    //limpar formulario
+    pessoaController._limpaForm()
+    
+})
--- poo/estudoAssincrono/js/aulasProfEd/pessoaController.js
+++ poo/estudoAssincrono/js/aulasProfEd/pessoaController.js
@@ -12,7 +12,7 @@ export class PessoaController {
     //metodos:
 
     //contructor:
-
+constructor(){
     this._inputNome = document.querySelector('#nome')
     this._inputIdade = document.querySelector('#idade')
     this._inputPeso = document.querySelector('#peso')
@@ -22,5 +22,46 @@ export class PessoaController {
     //this.listaPessoas = new ListaPessoas()
 
     //this._pessoasView = new PessoasView(document.querySelector)
+
+}
+
+// adcionar pessoa
+adiciona(event) {
+    event.preventDefault()
+
+    //criar uma pessoa
+    this._criaPessoa()
+    console.log(this._criaPessoa())
+
+    //adicionar nova pessoa na lista
+    //this.listaPessoas.adiciona(this._criaPessoa())
+    //console.log(this._listaPessoas.pessoas)
+
+    //this._pessoasView.update(this._listaPessoas)
+
+}
+
+//criar pessoa
+_criaPessoa() {
+    return new Pessoa(
+        this._inputNome.value,
+        this._inputIdade.value,
+        this._inputPeso.value,
+        this._inputAltura.value,
+
+    )
+}
+
+// limpar formulário
+_limpaForm() {
+    this._inputNome.value = ''
+    this._inputIdade.value = ''
+    this._inputPeso.value = ''
+    this._inputAltura.value = ''
+
+    this._inputNome.focus()
+}
+
+
 
 }
