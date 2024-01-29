29c2bbd7dfb0f27b255cb2087416605c3388f1a4
Sarah <sdmguerre@gmail.com>
2024-01-29 18:27:15 -0300

    comentei todo o codigo e tentei tornar os elementos de entrada obrigatorios, mas aparentemente pra que isso aconteça, preciso trocar a tag a do botao pela tag button

--- js_dom/entrega/app.js
+++ js_dom/entrega/app.js
@@ -1,6 +1,10 @@
+// Começo definindo as funções do projeto fora do meu evento principal pra que elas possam ser acessadas em qualquer parte do codigo. Essa primeira função usa uma expressão regular pra validar endereços de email, como pede orequisito 1. Se o preenchimento segue o padrão de um endereço de email, ele será considerado válido:
+
 function isValidEmail(email) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
+
+// A segunda função salva os valores preenchidos localmente:
 
 function saveFormValues() {
     const inputs = document.querySelectorAll('input');
@@ -8,6 +12,8 @@ function saveFormValues() {
         localStorage.setItem(input.id, input.value)
     })
 }
+
+// Já nessa função, os valores preenchidos anteriormente sao carregados, pra que o user não seja solicitado a preencher os campos novamente, como pede o requisito 1:
 
 function loadFormValues() {
     const inputs = document.querySelectorAll('input')
@@ -31,9 +37,13 @@ document.addEventListener('DOMContentLoaded', function(){
     titulo.classList.add('title-page')
     root.appendChild(titulo)
 
+// Aqui, eu crio uma div de class='calculator' para encapsular todo o conteúdo do formulário e uso o método isertBefore pra definir esta div como irmã do primeiro h1 da pagina, onde este é passado como argumento do metodo para defini-lo como irmão da div, ou seja, ela vira filha direta de root, assim como o h1 de churrascometro acima:
+
     const divCalculator = document.createElement('div');
     divCalculator.classList.add('calculator')
     root.insertBefore(divCalculator, titulo.nextSibling);
+
+// dentro da div calculator, tbm foi criada uma outra div que vai englobar tudo da pagina, menos o primeiro h1. Essa escolha foi feita pra seguir o template disponibilizado pelo professor e eu poder aproveitar o css feito por ele: 
 
     const divFormGroup = document.createElement('div');
     divFormGroup.classList.add('row', 'input-form-group')
@@ -43,6 +53,7 @@ document.addEventListener('DOMContentLoaded', function(){
     paragrafo.setAttribute('id', 'header-form')
     divFormGroup.appendChild(paragrafo)
 
+// Aqui foi criado um form pra englobar apenas os elementos de entrada do formulário:
 
     const form = document.createElement('form');
     
@@ -51,6 +62,7 @@ document.addEventListener('DOMContentLoaded', function(){
     nomeInput.setAttribute('id', 'nome');
     nomeInput.setAttribute('placeholder', 'Nome');
     nomeInput.classList.add('input-form');
+    nomeInput.setAttribute('required', true);
     form.appendChild(nomeInput);
 
     const emailInput = document.createElement('input');
@@ -58,6 +70,7 @@ document.addEventListener('DOMContentLoaded', function(){
     emailInput.setAttribute('id', 'email');
     emailInput.setAttribute('placeholder', 'E-mail');
     emailInput.classList.add('input-form');
+    emailInput.setAttribute('required', true);
     form.appendChild(emailInput);
     
     const cepInput = document.createElement('input');
@@ -65,8 +78,10 @@ document.addEventListener('DOMContentLoaded', function(){
     cepInput.setAttribute('id', 'cep');
     cepInput.setAttribute('placeholder', 'CEP');
     cepInput.classList.add('input-form');
+    cepInput.setAttribute('required', true);
     form.appendChild(cepInput);
 
+// inspecionando o codigo disponibilizado pelo prof, tbm notei que o input e a chekbox foram encapsuladas por uma div de ID='consent-input' então, tbm reproduzi isto: 
 
     const divConsent = document.createElement('div');
     divConsent.id = 'consent-input';
@@ -74,6 +89,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const consentimentoCheckbox = document.createElement('input');
     consentimentoCheckbox.setAttribute('type', 'checkbox');
     consentimentoCheckbox.setAttribute('id', 'consentimento');
+// Para que o checkbox seja carregado marcado automaticamente(requisito 1):    
     consentimentoCheckbox.checked = true;
     divConsent.appendChild(consentimentoCheckbox);
     
@@ -84,8 +100,8 @@ document.addEventListener('DOMContentLoaded', function(){
     divConsent.appendChild(consentimentoLabel);
     form.appendChild(divConsent)
     divFormGroup.appendChild(form)
-    /*root.appendChild(form)*/
 
+// Aqui foi criado uma div de class row pra encapsular o botao de cadastrar e uma variavel pra guardar este botao, então defini esta variavel como filha da divRow e a divRow como filha da divFormGroup inicializada acima:
 
     const divRow = document.createElement('div');
     divRow.classList.add('row');
@@ -98,9 +114,13 @@ document.addEventListener('DOMContentLoaded', function(){
     divRow.appendChild(buttonCadastrar);
     divFormGroup.appendChild(divRow);
 
+// Criei o evento de clique para o botao:    
+
     buttonCadastrar.addEventListener('click', function() {
         alert('cadastro realizado com sucesso!')
     })
+
+// e tbm criei uma div pra carregar os botoes de voltar e pular e organizei suas hierarquias dentro do codigo:
 
     const nav = document.createElement('div');
     nav.setAttribute('id', 'nav-container');
@@ -121,7 +141,11 @@ document.addEventListener('DOMContentLoaded', function(){
     divCalculator.appendChild(divFormGroup)
     root.appendChild(divCalculator)
 
+// O método que segue me permite cumprir o requisito "Uma vez que o usuário já tenha preenchido estes campos, não devemos solicitá-los novamente após a página ser recarregada", ele carrega os valores salvos no localStorage de volta para os campos do formulário:
+    
     loadFormValues();
+
+// É com o trecho de codigo abaixo que consigo fornecer um feedback visual em tempo real se os dados inseridos sao validos ou nao, como pedido no requisito 1:
 
     const inputs = [nomeInput, emailInput, cepInput];
     inputs.forEach(input => {
