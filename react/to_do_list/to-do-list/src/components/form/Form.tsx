24ea486f19a4667fd2d0275850822c6a484273ee
Sarah <sdmguerre@gmail.com>
2024-02-06 15:33:49 -0300

    deixando o form no jeito-

--- react/to_do_list/to-do-list/src/components/form/Form.tsx
+++ react/to_do_list/to-do-list/src/components/form/Form.tsx
@@ -1,16 +1,17 @@
 type FormProps = {
     nomeAtividade: string;
-    atualizarNomeAtividade:(novoNome)
+    atualizarNomeAtividade:(novoNome: string) => void;
     acaoSalvar: () => void;
 }
-//se o componente recebe propriedades, crie um type
+//se o componente recebe propriedades, crie um type com props
 
-const Form = ({nomeAtividade, acaoSalvar, }) => {
+const Form = ({nomeAtividade, atualizarNomeAtividade,acaoSalvar}:FormProps) => {
     return (
         <div className='Form'>
-              <input type='text' value={novaAtividade} onChange={(e) => setNovaAtividade}></input>
-              <button onClick={() => novaAtividadeHandler()} disabled={novaAtividade}>salvar</button>
-            </div>
+              <input type='text' value={nomeAtividade} onChange={(e) => atualizarNomeAtividade(e.target.value)}/>
+
+              <button onClick={() => acaoSalvar()} disabled={nomeAtividade.trim().length === 0}>salvar</button>
+        </div>
     )
 }
 
