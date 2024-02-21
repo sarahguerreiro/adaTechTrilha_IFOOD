e47bde30bfe0df7ea442079da3d59099a4f88d40
Sarah <sdmguerre@gmail.com>
2024-02-21 16:17:48 -0300

    para cada rota que vc tiver, adc uma tag Route e pra que ela possa ser usada, precisa ser chamada entre as chaves de import e, como estamos trabalhando com tags html, o arquivo de rotas precisa ser tsx, pois so sera usado o arquivo ts quando o documento tiver apenas tipagem

---
+++ react/bestBrowserGames/src/routes.tsx
@@ -0,0 +1,10 @@
+import { Route, BrowserRouter } from "react-router-dom";
+// apos baixar o pct de browserrouter, vc pode importar a tag BrowserRouter de react-router-dom
+// e é dentro do browser de rotas que ficarao as chamadas de rotas. Aqui rotas = paginas
+const Routes = () => {
+    return (<BrowserRouter>
+    <Route path= "" Component={}/>
+    <Route path= "" Component={}/>
+    <Route path= "" Component={}/>
+    </BrowserRouter>)
+}
