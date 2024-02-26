7dfe6559c1f9550ec6bc282a0ae045e9893c0840
Sarah <sdmguerre@gmail.com>
2024-02-26 10:40:34 -0300

    adiciona o tipo de login a types

--- react/bestBrowserGames/src/pages/Login/Login.tsx
+++ react/bestBrowserGames/src/pages/Login/Login.tsx
@@ -1,5 +1,33 @@
+import { LoginType } from "../../types/login.type";
+
 const Login = () => {
-    return <div>Estou logado!</div>
+
+    const realizarLogin = (novoUsuario: LoginType) => {
+        const listaAtual = localStorage.getItem('listaUsuarios');
+        let listaUsuariosCadastrados = [];
+
+        if (listaAtual) {
+            listaUsuarios = JSON.parse(listaAtual);
+        }
+    }
+
+    const submitHandler = (e: any) => {
+        e.preventDefault();
+        const loginUsuario: LoginType = {
+            email: e.target[0].value,
+            senha: e.target[1].value
+        }
+        realizarLogin(loginUsuario);
+    }
+
+    return <div>
+        <h2>Login</h2>
+        <form>
+            <input type="email" placeholder="email"/>
+            <input type="password" placeholder="senha"/>
+            <button>Acessar</button>
+        </form>
+    </div>
 }
 
 export default Login
