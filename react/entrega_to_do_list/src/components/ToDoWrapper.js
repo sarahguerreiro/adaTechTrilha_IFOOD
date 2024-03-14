d0bf2532e48c336fb45f6f86a932e63df462efa5
Sarah <sdmguerre@gmail.com>
2024-03-14 20:11:58 -0300

    hoje meu minimo foi meu maximo

---
+++ react/entrega_to_do_list/src/components/ToDoWrapper.js
@@ -0,0 +1,12 @@
+import  React  from "react";
+import  {ToDoForm}  from "./TodoForm";
+
+export const ToDoWrapper = () => {
+  return (
+    <div className="ToDoWrapper">
+      <ToDoForm /> 
+    </div>
+  )
+}
+
+export default ToDoWrapper
