5e9f00e09e002830d487a1656d28eda7982a9d5e
Sarah <sdmguerre@gmail.com>
2024-03-15 21:44:33 -0300

    Entrega: To Do List

--- react/entregaToDoList/src/App.tsx
+++ react/entregaToDoList/src/App.tsx
@@ -1,7 +1,22 @@
 import { Header } from "./components/Header/Header"
 import Tasks from "./components/Tasks/Tasks"
+import useState from 'react'
+
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  // funcao q permite adc tasks a minha lista de
+  function addTask(taskTitle) {
+    setTasks([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isCompleted: false
+      }
+    ]);
+  }
+
   return (
     <>
       <Header />
